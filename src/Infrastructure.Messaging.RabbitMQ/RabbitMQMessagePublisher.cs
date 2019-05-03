17da23ca81e2f1b6f72b3808d002858239e23060
dmitriy afonin <dmitriy.afonin@walletone.com>
2019-05-04 01:42:42 +0500

    Infrastructure.Messaging.RabbitMQ was added.

---
+++ src/Infrastructure.Messaging.RabbitMQ/RabbitMQMessagePublisher.cs
@@ -0,0 +1,74 @@
+﻿namespace Infrastructure.Messaging.RabbitMQ
+{
+    using global::RabbitMQ.Client;
+    using global::RabbitMQ.Client.Exceptions;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public sealed class RabbitMQMessagePublisher : IMessagePublisher
+    {
+        private readonly Lazy<IConnection> _connection;
+        private readonly Lazy<IModel> _publishChannel;
+        private readonly TaskFactory _taskFactory;
+        private readonly ILoggerFactory _loggerFactory;
+        private readonly IMessagePacker _messagePacker;
+
+        public RabbitMQMessagePublisher(
+            ILoggerFactory loggerFactory,
+            TaskFactory taskFactory,
+            IMessagePacker messagePacker,
+            IConnectionFactory connectionFactory)
+        {
+            _taskFactory = taskFactory;
+            _messagePacker = messagePacker;
+            _loggerFactory = loggerFactory;
+            _connection = _connection = new Lazy<IConnection>(() =>
+            {
+                var logger = _loggerFactory.CreateLogger<RabbitMQMessagePublisher>();
+
+                var tryCount = 20;
+                var timeOut = TimeSpan.FromSeconds(4);
+
+                while (tryCount != 0)
+                {
+                    Interlocked.Decrement(ref tryCount);
+                    try
+                    {
+                        var connection = connectionFactory.CreateConnection();
+                        logger.LogInformation("Connection is succesfull => {connection}", connection);
+                        return connection;
+                    }
+                    catch (BrokerUnreachableException e)
+                    {
+                        logger.LogError("Can't connect to the RabbitMq bus. Trying to reconnect in {timeOut} | Try[{tryCount}]. Exception {e}", timeOut, tryCount, e);
+                        Task.Delay(timeOut).GetAwaiter().GetResult();
+                    }
+                }
+                throw new Exception("No connection to the event bus");
+            });
+            _publishChannel = new Lazy<IModel>(() => _connection.Value.CreateModel());
+        }
+
+        public Task Publish(IMessage message, CancellationToken cancellationToken)
+        {
+            var eventType = message.GetType();
+            var exchangeName = eventType.Namespace.Split('.').First(); //TODO: exchangeName 
+            var routingKey = eventType.Name;
+            var queueName = $"{exchangeName}.{routingKey}";
+
+            _publishChannel.Value.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Direct);
+            _publishChannel.Value.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false);
+            _publishChannel.Value.QueueBind(queue: queueName, exchange: exchangeName, routingKey: routingKey);
+
+            return _taskFactory.StartNew(() =>
+            {
+                var packedMessage = (string)_messagePacker.Pack(message, cancellationToken).GetAwaiter().GetResult();
+                _publishChannel.Value.BasicPublish(exchange: exchangeName, routingKey: routingKey, body: Encoding.UTF8.GetBytes(packedMessage));
+            }, cancellationToken);
+        }
+    }
+}
---
+++ tests/Infrastructure.Messaging.Tests/RabbitMQTests.cs
@@ -0,0 +1,54 @@
+﻿using Infrastructure.Messaging.RabbitMQ;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Infrastructure.Messaging.Tests
+{
+    public sealed class RabbitMQTests
+    {
+        public sealed class TestMessage : IMessage
+        {
+            public string Ping { get; set; } = "Ping";
+        }
+        public sealed class TestMessageHandler : IMessageHandler<TestMessage>
+        {
+            public Task Handle(TestMessage message, CancellationToken cancellationToken)
+            {
+                message.Ping = "Pong";
+                return Task.CompletedTask;
+            }
+        }
+
+        [Fact]
+        public void When_Published_Succsessfully_With_Json_Then_True()
+        {
+            var services = new ServiceCollection();
+
+            var connectionFactory = new ConnectionFactory { Uri = new Uri("######") };
+
+            services
+                .AddLogging()
+                .AddSingleton<IConnectionFactory>(connectionFactory)
+                .AddSingleton<IMessagePacker, JsonMessagePacker>()
+                .AddSingleton<IMessagePublisher, RabbitMQMessagePublisher>()
+                .AddSingleton<TaskFactory>()
+                .AddSingleton<IMessageHandler<TestMessage>, TestMessageHandler>()
+                .AddSingleton<IMessageHandlersRegistry, MessageHandlersRegistryIoC>();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var publisher = serviceProvider.GetRequiredService<IMessagePublisher>();
+
+            var testMessage = new TestMessage();
+
+            publisher.Publish(testMessage, CancellationToken.None).GetAwaiter().GetResult();
+        }
+    }
+}
